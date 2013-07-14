648c76f79ec87e56e97ddf36bab63cc6b293d636
brettkwarner <bkwarner22@gmail.com>
2013-07-14 03:51:44 +0000

    test update

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var inputfile = fs.readFileSync('/index.html);'
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString(inputfile);
 });
 
 var port = process.env.PORT || 5000;
